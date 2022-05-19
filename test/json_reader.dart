cf4d73dec3481ad3dc2838726bd2cebf274f98b0
anindyadesi <anindya.desi@gmail.com>
2022-05-19 11:44:48 +0700

    adding detail so test can run from root folder so we can use codemagic with modular project in it. 2

--- test/json_reader.dart
+++ test/json_reader.dart
@@ -5,5 +5,17 @@ String readJson(String name) {
   if (dir.endsWith('/test')) {
     dir = dir.replaceAll('/test', '');
   }
+  if (dir.endsWith('core')) {
+    return File('$dir/test/$name').readAsStringSync();
+  }
+  if (dir.endsWith('detail')) {
+    return File('$dir/test/$name').readAsStringSync();
+  }
+  if (dir.endsWith('search')) {
+    return File('$dir/test/$name').readAsStringSync();
+  }
+  if (dir.endsWith('watchlist')) {
+    return File('$dir/test/$name').readAsStringSync();
+  }
   return File('$dir/test/$name').readAsStringSync();
 }
